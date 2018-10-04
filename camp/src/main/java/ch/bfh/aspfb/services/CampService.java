d55f9d751002fed311e783e586fdb66f1469e758
Patrick Hirt <patrick.hirt@swisscom.com>
2018-10-04 17:43:23 +0200

    lol uml

---
+++ camp/src/main/java/ch/bfh/aspfb/services/CampService.java
@@ -0,0 +1,26 @@
+package ch.bfh.aspfb.services;
+
+import ch.bfh.aspfb.entities.Hero;
+import ch.bfh.aspfb.entities.Party;
+
+import java.util.List;
+
+public class CampService {
+    private List<? extends Party> partyList;
+
+    public void joinParty(Hero hero, Party party) {
+
+    }
+
+    public void addParty(Party party) {
+
+    }
+
+    public void quitParty(Hero hero, Party party) {
+
+    }
+
+    public List<? extends Party> getPartyList() {
+        return partyList;
+    }
+}
