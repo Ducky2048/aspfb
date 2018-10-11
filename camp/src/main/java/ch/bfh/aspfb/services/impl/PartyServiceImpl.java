81dd26195a88a4d6eaf0f9765a54d3dc607333ad
Patrick Hirt <patrick.hirt@gmail.com>
2018-10-11 17:59:31 +0200

    lol spring impl

---
+++ camp/src/main/java/ch/bfh/aspfb/services/impl/PartyServiceImpl.java
@@ -0,0 +1,25 @@
+package ch.bfh.aspfb.services.impl;
+
+import ch.bfh.aspfb.model.Party;
+import ch.bfh.aspfb.service.HeroService;
+import ch.bfh.aspfb.service.PartyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+
+@Service
+public class PartyServiceImpl implements PartyService {
+    @Autowired
+    private HeroService heroService;
+
+    public Party createParty(String name) {
+        final Party party = new Party();
+        party.setName(name);
+        party.setMembers(IntStream.range(0, 4).mapToObj(i -> this.heroService.createHero(String.format("Le hero %d", i))).collect(Collectors.toList()));
+        System.out.println(String.format("Created Party: %s", party.toString()));
+        return party;
+    }
+}
