981b9abf11a221b242c80e6a5696002898ddc449
Gabor Tanz <izolight@gmail.com>
2018-11-01 17:18:22 +0100

    forgot to add extends

--- camp/src/main/java/ch/bfh/aspfb/repository/HeroRepository.java
+++ camp/src/main/java/ch/bfh/aspfb/repository/HeroRepository.java
@@ -1,7 +1,9 @@
 package ch.bfh.aspfb.repository;
 
+import ch.bfh.aspfb.model.Hero;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface HeroRepository {
+public interface HeroRepository extends CrudRepository<Hero, String> {
 }
--- camp/src/main/java/ch/bfh/aspfb/repository/PartyRepository.java
+++ camp/src/main/java/ch/bfh/aspfb/repository/PartyRepository.java
@@ -1,7 +1,9 @@
 package ch.bfh.aspfb.repository;
 
+import ch.bfh.aspfb.model.Party;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PartyRepository {
+public interface PartyRepository extends CrudRepository<Party, String> {
 }
