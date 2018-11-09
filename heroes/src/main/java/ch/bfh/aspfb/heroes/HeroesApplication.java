72e3c2686d73b8de316bfefd207a53ce832843ef
Patrick Hirt <patrick.hirt@swisscom.com>
2018-11-09 10:01:30 +0100

    lol repository populator

--- heroes/src/main/java/ch/bfh/aspfb/heroes/HeroesApplication.java
+++ heroes/src/main/java/ch/bfh/aspfb/heroes/HeroesApplication.java
@@ -2,8 +2,10 @@ package ch.bfh.aspfb.heroes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource({"classpath:application-context.xml"})
 public class HeroesApplication {
 
 	public static void main(String[] args) {
