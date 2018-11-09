b59b3b9dc3499b29368b73e1da14c264f89e0ea9
Patrick Hirt <patrick.hirt@swisscom.com>
2018-11-09 15:07:32 +0100

    y u no werk

---
+++ heroes/src/main/java/ch/bfh/aspfb/heroes/rest/controller/PartyController.java
@@ -0,0 +1,48 @@
+package ch.bfh.aspfb.heroes.rest.controller;
+
+import ch.bfh.aspfb.heroes.model.Hero;
+import ch.bfh.aspfb.heroes.model.Party;
+import ch.bfh.aspfb.heroes.repository.PartyRepository;
+import ch.bfh.aspfb.heroes.service.HeroService;
+import ch.bfh.aspfb.heroes.service.PartyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.hateoas.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@RepositoryRestController
+public class PartyController {
+    @Autowired
+    private PartyService partyService;
+
+    @Autowired
+    private PartyRepository partyRepository;
+
+    @Autowired
+    private HeroService heroService;
+
+
+    @RequestMapping(method = RequestMethod.POST, value = "dynamicPartyCreation")
+    public @ResponseBody
+    ResponseEntity<Party> dynamicPartyCreation(@RequestBody(required = true) Resource<Party> partyResource) {
+        if (partyResource == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        final Party party = this.partyService.createParty(partyResource.getContent().getName());
+        final List<Hero> heroes = IntStream.range(0, 3)
+                .mapToObj(i -> this.heroService.createHero(String.format("Hero %d for %s", i, party.getName())))
+                .collect(Collectors.toList());
+        party.setMembers(heroes);
+        this.partyRepository.save(party);
+        return ResponseEntity.ok(party);
+    }
+}
