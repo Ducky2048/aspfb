ec82411594733fbdf8f8d1c23ca3b9e126480bfb
Bernard Jaquet <b.jaquet@gmx.ch>
2018-11-14 15:26:10 +0100

    Try to remove old repositories

--- heroes/src/main/java/ch/bfh/aspfb/heroes/repository/PartyRepository.java
+++
@@ -1,9 +0,0 @@
-package ch.bfh.aspfb.heroes.repository;
-
-import ch.bfh.aspfb.heroes.model.Party;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PartyRepository extends CrudRepository<Party, String> {
-}
