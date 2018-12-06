f77e54ee8a46b61846a659c6aed2ccbd41f2367d
Patrick Hirt <patrick.hirt@swisscom.com>
2018-12-06 16:42:23 +0100

    possibly fix shit, possibly break shit

--- camp/src/test/java/ch/bfh/aspfb/camp/CampApplicationTests.java
+++ camp/src/test/java/ch/bfh/aspfb/camp/CampApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class HeroesApplicationTests {
+public class CampApplicationTests {
 
 	@Test
 	public void contextLoads() {
