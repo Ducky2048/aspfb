21e78ceb4ecff3981805d99c529a1e759efabc6c
Patrick Hirt <patrick.hirt@swisscom.com>
2018-12-08 15:24:03 +0100

    hass

--- camp/src/main/java/ch/bfh/aspfb/camp/rest/controller/PartyController.java
+++ camp/src/main/java/ch/bfh/aspfb/camp/rest/controller/PartyController.java
@@ -8,21 +8,16 @@ import ch.bfh.aspfb.camp.service.PartyService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.data.rest.webmvc.support.RepositoryEntityLinks;
 import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
-@RepositoryRestController
-@Configuration
+@RestController
 public class PartyController {
     private static final Logger logger = LoggerFactory.getLogger(PartyController.class);
 
@@ -39,21 +34,16 @@ public class PartyController {
     private RepositoryEntityLinks entityLinks;
 
 
-    @GetMapping(value = "/parties/dynamicPartyCreation")
+    @GetMapping(value = "/dynamicPartyCreation")
     public Party dynamicPartyCreation(@NonNull @RequestParam final String name) {
 
         final Party party = this.partyService.createParty(name);
-
         party.add(linkTo(methodOn(PartyController.class).dynamicPartyCreation(name)).withSelfRel());
 
-        party.setMembers(
-                IntStream.range(0, 3)
-                .mapToObj(i -> this.heroService.createHero(String.format("Hero %d for %s", i, party.getName())))
-                .collect(Collectors.toList())
-        );
+        for(int i = 0; i < party.getMembers().size(); i++) {
+            party.add(this.entityLinks.linkToSingleResource(Hero.class, party.getMembers().get(i).getId()).withRel(String.format("Hero no %d", i)));
+        }
 
-        party.getMembers().forEach(h -> party.add(this.entityLinks.linkToSingleResource(Hero.class, h.getId()).withSelfRel()));
         return party;
-
     }
 }
--- promoter/src/main/java/ch/bfh/aspfb/promoter/repo/CampClient.java
+++ promoter/src/main/java/ch/bfh/aspfb/promoter/repo/CampClient.java
@@ -2,12 +2,12 @@ package ch.bfh.aspfb.promoter.repo;
 
 import ch.bfh.aspfb.promoter.model.Party;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(value = "camp", fallback = CampClientFallback.class)
 public interface CampClient {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/parties/dynamicPartyCreation")
-    Party createParty(final String name);
+    @GetMapping(value = "/dynamicPartyCreation")
+    Party createParty(@RequestParam("name") final String name);
 }
